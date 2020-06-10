6398070929103f91bef948e4d9a1ed8bb8b958e9
ovanderzee <hastalavista@dds.nl>
2020-06-10 16:56:26 +0200

    maintenance

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 // rollup.config.js
 import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 
 const name = "documentStaging";
